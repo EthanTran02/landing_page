59b344c3135d6a12dc27299bedf130521aee1950
Ethan <khoitranec@gmail.com>
2024-11-19 22:49:45 +0700

    Make it a game of 5 round

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+
+
+let humanScore = 0
+let computerScore  = 0
+
+function playRound (humanChoice, computerChoice) {
+    if (humanChoice == 'rock' && computerChoice == 'scissor' 
+        || humanChoice == 'paper' && computerChoice == 'rock' 
+        || humanChoice == 'scissor' && computerChoice == 'paper') {
+        console.log(`You win! ${humanChoice} beat ${computerChoice}`)
+        humanScore += 1
+    } else if (humanChoice == computerChoice){
+        console.log('It a tie!')
+    } else {
+        console.log(`You lose! ${computerChoice} beat ${humanChoice}`)
+        computerScore += 1
+    }
+}
+
+let humanSelection = getHumanChoice().toLowerCase()
+let computerSelection = getComputerChoice().toLowerCase()
+
+function playGame (){
+    for (let i = 0; i < 5; i++) 
+        playRound(getHumanChoice(), getComputerChoice())
+}
+
+playGame()
+
+console.log(`${humanScore}  ${computerScore}`)
+
+
 function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * 3)
     if (randomNum == 1) {
@@ -10,25 +42,6 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let input = prompt('type: rock, paper, or scissor')
+    let input = prompt('type rock, paper, or scissor:')
     return input
 }
-
-let humanScore = 0
-let computerScore  = 0
-
-function playRound (humanChoice, computerChoice) {
-    if (humanChoice == 'rock' && computerChoice == 'scissor' 
-        || humanChoice == 'paper' && computerChoice == 'rock' 
-        || humanChoice == 'scissor' && computerChoice == 'paper') {
-        console.log(`You win! ${humanChoice} beat ${computerChoice}`)
-        humanScore += 1
-    } else {
-        console.log(`You lose! ${computerChoice} beat ${humanChoice}`)
-        computerScore += 1
-    }
-}
-
-let humanSelection = getHumanChoice().toLowerCase()
-let computerSelection = getComputerChoice().toLowerCase()
-
