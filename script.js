7a07b17ea3fc6d5fb6645383ba770f63629b3a56
Ethan <khoitranec@gmail.com>
2024-11-19 16:50:14 +0700

    game can play a single round

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function getHumanChoice() {
     let input = prompt('type: rock, paper, or scissor')
     return input
 }
+
+let humanScore = 0
+let computerScore  = 0
+
+function playRound (humanChoice, computerChoice) {
+    if (humanChoice == 'rock' && computerChoice == 'scissor' || humanChoice == 'paper' && computerChoice == 'rock' || humanChoice == 'scissor' && computerChoice == 'paper') {
+        console.log(`You win! ${humanChoice} beat ${computerChoice}`)
+        humanScore += 1
+    } else {
+        console.log(`You lose! ${computerChoice} beat ${humanChoice}`)
+        computerScore += 1
+    }
+}
+
+let humanSelection = getHumanChoice().toLowerCase()
+let computerSelection = getComputerChoice().toLowerCase()
+
