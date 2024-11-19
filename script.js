8958801741b415a195c374111b533adf85d7a467
Ethan <khoitranec@gmail.com>
2024-11-19 22:50:46 +0700

    re commit (finished game function)

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+function getComputerChoice() {
+    let randomNum = Math.floor(Math.random() * 3)
+    if (randomNum == 1) {
+        return 'rock'
+    } else if ( randomNum == 2) {
+        return 'paper'
+    } else {
+        return 'scissor'
+    }
+}
 
+function getHumanChoice() {
+    let input = prompt('type rock, paper, or scissor:')
+    return input
+}
 
 let humanScore = 0
 let computerScore  = 0
@@ -28,20 +42,3 @@ function playGame (){
 playGame()
 
 console.log(`${humanScore}  ${computerScore}`)
-
-
-function getComputerChoice() {
-    let randomNum = Math.floor(Math.random() * 3)
-    if (randomNum == 1) {
-        return 'rock'
-    } else if ( randomNum == 2) {
-        return 'paper'
-    } else {
-        return 'scissor'
-    }
-}
-
-function getHumanChoice() {
-    let input = prompt('type rock, paper, or scissor:')
-    return input
-}
