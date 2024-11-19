2877ab9bdb21d21d48116fb029843309ae190f2e
Ethan <khoitranec@gmail.com>
2024-11-19 23:39:38 +0700

    Fix typo

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * 3)
-    if (randomNum == 1) {
+    if ( randomNum == 1 ) {
         return 'rock'
-    } else if ( randomNum == 2) {
+    } else if ( randomNum == 2 ) {
         return 'paper'
     } else {
         return 'scissor'
